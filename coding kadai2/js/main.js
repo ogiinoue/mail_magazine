d58a7f315ca8a037c362b7cea14d2f405c1f8b04
Ogino <ndcogi@gmail.com>
2025-03-09 22:20:32 +0900

    Delete coding kadai2/js directory

--- coding kadai2/js/main.js
+++
@@ -1,6 +0,0 @@
-//ハンバーガーメニュの記述
-
-$('.burger-btn').on('click',function(){ //"burger-btn"をクリックしたら、
-  $('.burger-btn').toggleClass('cross'); //ハンバーガーボタンに"cross"というclassをつけたり外したりする
-  $('.header-nav').fadeToggle(300); //ヘッダーナビゲーションを300ミリ秒でフェードイン、フェードアウトさせる
-});
--- coding kadai2/js/script.js
+++
@@ -1,7 +0,0 @@
-//ハンバーガーメニュの記述
-
-$('.hamburger_menu').on('click',function(){ //"hamburger_menu"をクリックしたら、
-  $('.hamburger_menu').toggleClass('cross'); //ハンバーガーボタンに"cross"というclassをつけたり外したりする
-  $('.header__block').fadeToggle(300); //ヘッダーナビゲーションを300ミリ秒でフェードイン、フェードアウトさせる
-  $('body').toggleClass('noscroll') //bodyに対して"noscroll"というclassをつけたり外したりする
-});
